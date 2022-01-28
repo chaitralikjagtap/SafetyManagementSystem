c8a8185189266758e820b3c78e5821ef58f44851
Jagtap <chaitrali.jagtap@capgemini.com>
2022-01-28 14:52:32 +0530

    First commit

---
+++ src/main/java/com/safety/SafetyManagementSystem1Application.java
@@ -0,0 +1,13 @@
+package com.safety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyManagementSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyManagementSystem1Application.class, args);
+	}
+
+}
